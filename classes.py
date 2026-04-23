10a968104416ef4cf4da35bc04a7d2b744367546
MekhmanPashaev <mehmanpashaev2006@gmail.com>
2026-04-23 20:01:50 +0300

    Added classes

---
+++ classes.py
@@ -0,0 +1,20 @@
+class natural:
+    def __init__(self, data : list):
+        self.data = data.copy()
+        self.n = len(data) + 1
+
+class integer():
+    def __init__(self, sign :  int, data : natural):
+        self.sign = sign
+        self.data = data
+
+class rational:
+    def __init__(self, numerator : integer, denominator : natural):
+        self.numerator = numerator
+        self.denominator = denominator
+
+class polynom:
+    def __init__(self, coef : list):
+        self.coef = coef.copy()
+        self.deg = len(coef)
+
