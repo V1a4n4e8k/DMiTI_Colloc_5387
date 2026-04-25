7a7aa36a7c19876dcd3600550167ba981a5f1800
iaspitsyn_25 <sivans031907@gmail.com>
2026-04-25 11:47:58 +0000

    func 1-3

--- unsig_int.py
+++ unsig_int.py
@@ -20,3 +20,32 @@ sub(a,b)
 
 print(print(a))
 """
+# task 1 !!!!!! a, b - NUMBERS 0-9 arrays!
+def compare_unsig_int(a, b):
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            return 1
+        if i >= len(b):
+            return 1
+    return 0
+""" use ex
+print(compare_unsig_int([3,2,3,4],[2,3,4]))
+"""
+def is_zero(a):
+    if len(a) == 0 and a[0] == 0:
+        return 0
+    return 1
+""" use ex
+print(is_zero([0]))
+"""
+def add_one(a):
+    i = len(a)-1
+    while i >= 0:
+        if a[i] != 9:
+            a[i] += 1
+            break
+        i -= 1
+""" use ex
+a = [1][9]
+print(add_one(a))
+"""
