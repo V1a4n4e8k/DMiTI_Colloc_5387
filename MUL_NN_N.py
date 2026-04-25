df1284566897cb71166c99455dba2e20bdb8ce10
iaspitsyn_25 <sivans031907@gmail.com>
2026-04-25 12:40:22 +0000

    MUL_NN_N.py

---
+++ MUL_NN_N.py
@@ -0,0 +1,16 @@
+def MUL_NN_N(a, n):
+    for i in range(len(a)):
+        a[i] = a[i] * n
+    move = 0
+    for i in range(len(a) - 1, -1, -1):
+        a[i] += move
+        move = a[i] // 10
+        a[i] %= 10
+    if move:
+        a.insert(0, move)
+""" use ex
+a = [1,2,3]
+n = 9
+MUL_NN_N(a,n)
+print(a)
+"""
