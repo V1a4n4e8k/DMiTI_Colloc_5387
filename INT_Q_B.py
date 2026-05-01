11717a313cca42eece85267d4985374bbb3ff908
MekhmanPashaev <mehmanpashaev2006@gmail.com>
2026-05-02 00:19:33 +0300

    Исправил Q-2 (Проверка на целое рационального числа) Тесты проходят

--- INT_Q_B.py
+++ INT_Q_B.py
@@ -1,7 +1,14 @@
+"""
+Q-2
+Проверка сокращенного дробного на целое, если рациональное число является целым, то «да», иначе «нет»
+"""
+
 from classes import *
-from COM_NN_D import *
+from MOD_NN_N import MOD_NN_N
+from NZER_N_B import NZER_N_B
 
 def INT_Q_B(a : rational):
-    if COM_NN_D(a.denominator.data, natural([1])) == 0:
+    ost = MOD_NN_N(a.numerator.data, a.denominator)
+    if NZER_N_B(ost) == 0:
         return 1
     return 0
